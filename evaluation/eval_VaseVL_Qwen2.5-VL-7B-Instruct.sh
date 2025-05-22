9e5d82fc1480cc0e08e1af98e09be6568fc89461
jc <jinchao.ge.pro@gmail.com>
2025-05-23 00:25:48 +0930

    add Qwen2.5-VL-7B-Instruct

---
+++ evaluation/eval_VaseVL_Qwen2.5-VL-7B-Instruct.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INFER_FILE="results/VaseVL_Qwen2.5-VL-7B-Instruct/VaseVL_Qwen2.5-VL-7B-Instruct_inference_answers.jsonl"
+python evaluation/vqa_inference.py \
+    --model-path "Qwen/Qwen2.5-VL-7B-Instruct" \
+    --question-file "data/VaseVLDataset/vasevl_single_questions.json" \
+    --output-file $INFER_FILE \
+    --batch-size 80
+
+
+# --annotation-file use_default_annotation_file
+python evaluation/vqa_evaluation.py \
+    --infer-file  $INFER_FILE \
+    --result-file "results/VaseVL_Qwen2.5-VL-7B-Instruct/VaseVL_Qwen2.5-VL-7B-Instruct_evaluation.txt"
