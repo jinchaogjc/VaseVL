e0091cc6538c328ff1b10b1e72cdfb25afe6ba79
jc <jinchao.ge.pro@gmail.com>
2025-05-23 22:19:31 +0930

    add sft qwen2.5-vl-7b-instruct

---
+++ evaluation/eval_VaseVL_Qwen2.5-VL-7B-Instruct_vasevl_dataset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INFER_FILE="results/VaseVL_Qwen2.5-VL-7B-Instruct/VaseVL_Qwen2.5-VL-7B-Instruct_vasevl_dataset_inference_answers.jsonl"
+python evaluation/vqa_inference.py \
+    --model-path "/inspire/hdd/ws-ba572160-47f8-4ca1-984e-d6bcdeb95dbb/a100-maybe/wangbaode/Codes/LLaMA-Factory/saves/qwen2_5vl_7b_full_sft_vasevl_dataset/full/sft" \
+    --question-file "data/VaseVLDataset/vasevl_single_questions.json" \
+    --output-file $INFER_FILE \
+    --batch-size 80
+
+
+# --annotation-file use_default_annotation_file
+python evaluation/vqa_evaluation.py \
+    --infer-file  $INFER_FILE \
+    --result-file "results/VaseVL_Qwen2.5-VL-7B-Instruct/VaseVL_Qwen2.5-VL-7B-Instruct_vasevl_dataset_evaluation.txt"
