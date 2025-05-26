9eb5b54d0a9553e9624af569951bf6c87f5b7105
jc <jinchao.ge.pro@gmail.com>
2025-05-26 20:00:23 +0930

    add r1 404 batch_size 80

--- evaluation/eval_VaseVL_Qwen2.5-VL-3B-GRPO-VASEL-ZERO-R1-404.sh
+++ evaluation/eval_VaseVL_Qwen2.5-VL-3B-GRPO-VASEL-ZERO-R1-404.sh
@@ -5,7 +5,7 @@ python evaluation/vqa_inference.py \
     --model-path "/inspire/hdd/ws-ba572160-47f8-4ca1-984e-d6bcdeb95dbb/a100-maybe/wangbaode/NIPS_2025/Agent/VLM-R1-Toxic/src/open-r1-multimodal/output/Qwen2.5-VL-3B-GRPO-VASEL-ZERO-R1-404" \
     --question-file "data/VaseVLDataset/vasevl_single_questions.json" \
     --output-file $INFER_FILE \
-    --batch-size 1
+    --batch-size 80
 
 
 # --annotation-file use_default_annotation_file
