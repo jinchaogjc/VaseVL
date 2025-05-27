d6ccbaa9b8c32c126628829099a7db99ef485a74
jc <jinchao.ge.pro@gmail.com>
2025-05-27 21:18:45 +0930

    fix batch size not inference

---
+++ evaluation/eval_VaseVL_Qwen2-VL-0.5B-Instruct.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INFER_FILE="results/VaseVL_Qwen2-VL-0.5B/VaseVL_Qwen2-VL-0.5B_inference_answers.jsonl"
+
+python evaluation/vqa_inference.py \
+    --model-path "llava-hf/llava-onevision-qwen2-0.5b-ov-hf" \
+    --question-file "data/VaseVLDataset/vasevl_single_questions.json" \
+    --output-file $INFER_FILE \
+    --batch-size 80
+
+# --annotation-file use_default_annotation_file
+python evaluation/vqa_evaluation.py \
+    --infer-file  $INFER_FILE \
+    --result-file "results/VaseVL_Qwen2-VL-0.5B/VaseVL_Qwen2-VL-0.5B_evaluation.txt"
