715e07e01e8fff94d40aad8b9229e0aa7c81c16d
jc <jinchao.ge.pro@gmail.com>
2025-05-27 23:51:57 +0930

    update qwen2 0.5b path

--- evaluation/eval_VaseVL_Qwen2-VL-0.5B-Instruct.sh
+++ evaluation/eval_VaseVL_Qwen2-VL-0.5B-Instruct.sh
@@ -2,8 +2,9 @@
 
 INFER_FILE="results/VaseVL_Qwen2-VL-0.5B/VaseVL_Qwen2-VL-0.5B_inference_answers.jsonl"
 
+# --model-path "/inspire/hdd/ws-ba572160-47f8-4ca1-984e-d6bcdeb95dbb/a100-maybe/wangbaode/NIPS_2025/Codes/models/llava-onevision-qwen2-0.5b-ov-hf" \
 python evaluation/vqa_inference.py \
-    --model-path "/inspire/hdd/ws-ba572160-47f8-4ca1-984e-d6bcdeb95dbb/a100-maybe/wangbaode/NIPS_2025/Codes/models/llava-onevision-qwen2-0.5b-ov-hf" \
+    --model-path "llava-hf/llava-onevision-qwen2-0.5b-ov-hf" \
     --question-file "data/VaseVLDataset/vasevl_single_questions_no_tag.json" \
     --output-file $INFER_FILE \
     --batch-size 80
