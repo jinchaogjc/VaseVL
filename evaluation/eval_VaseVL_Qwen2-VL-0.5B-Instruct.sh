a0b8a8ef9f30e1cca60fc2a447e23f3bdaaf987f
jc <jinchao.ge.pro@gmail.com>
2025-05-28 13:10:36 +0930

    fix qwen2 0.5b err

--- evaluation/eval_VaseVL_Qwen2-VL-0.5B-Instruct.sh
+++ evaluation/eval_VaseVL_Qwen2-VL-0.5B-Instruct.sh
@@ -6,7 +6,7 @@ python evaluation/vqa_inference.py \
     --model-path "/inspire/hdd/ws-ba572160-47f8-4ca1-984e-d6bcdeb95dbb/a100-maybe/wangbaode/NIPS_2025/Codes/models/llava-onevision-qwen2-0.5b-ov-hf" \
     --question-file "data/VaseVLDataset/vasevl_single_questions_no_tag.json" \
     --output-file $INFER_FILE \
-    --batch-size 80
+    --batch-size 1
 
 # --annotation-file use_default_annotation_file
 python evaluation/vqa_evaluation.py \
